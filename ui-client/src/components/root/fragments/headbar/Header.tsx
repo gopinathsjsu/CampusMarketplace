7cacf1edfcea98f5d4edd9957b7a3517377cbc12
Seth McCarthy <emperorsheevpalpatine0@gmail.com>
2025-09-25 15:41:14 -0700

    chore(ui-client): reset

--- ui-client/src/components/root/fragments/headbar/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header style={{ padding: "12px 16px", borderBottom: "1px solid #e5e7eb" }}>
-      <h1 style={{ margin: 0, fontSize: 18 }}>Campus Marketplace</h1>
-    </header>
-  );
-}
-
-

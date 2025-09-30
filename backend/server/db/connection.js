993393920cd814ad133653100e6f0aba24b4387d
Faye Yang <74108896+Fyang094@users.noreply.github.com>
2025-09-29 22:24:17 -0700

    Add server API endpoints

--- backend/server/db/connection.js
+++ backend/server/db/connection.js
@@ -1,6 +1,7 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const uri = process.env.ATLAS_URI || "";
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -8,5 +9,19 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     },
 });
+
+try {
+  // Connect the client to the server
+  await client.connect();
+  // Send a ping to confirm a successful connection
+  await client.db("admin").command({ ping: 1 });
+  console.log(
+   "Pinged your deployment. You successfully connected to MongoDB!"
+  );
+} catch(err) {
+  console.error(err);
+}
+
+let db = client.db("employees");
 
 export default db

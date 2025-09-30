97bc346185ff49e87d27ffbdd78bf4ed8526155b
Faye Yang <74108896+Fyang094@users.noreply.github.com>
2025-09-29 21:32:02 -0700

    Setting up cors, express, mongodb

---
+++ backend/server/db/connection.js
@@ -0,0 +1,12 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const uri = process.env.ATLAS_URI || "";
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+export default db
